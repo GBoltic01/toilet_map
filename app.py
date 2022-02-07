53c85b8cb6dc2b4aae0897b3847ffb1a377b6e3f
Gregor Boltic <gregor.boltic@gmail.com>
2022-02-07 19:01:53 +0000

    hideen postgres and mailtrap credentials in app.py

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ import json, os, string, random
 
 app = Flask(__name__) 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:21363115g@localhost/Glasgow_Toilet_Map'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'xxx'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['MAIL_SERVER']='smtp.mailtrap.io'
 app.config['MAIL_PORT'] = 2525
-app.config['MAIL_USERNAME'] = 'd53644964ee663'
-app.config['MAIL_PASSWORD'] = '620db34e05d446'
+app.config['MAIL_USERNAME'] = 'xxx'
+app.config['MAIL_PASSWORD'] = 'xxx'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
